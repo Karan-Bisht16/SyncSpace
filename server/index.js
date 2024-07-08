13913747c33c44b29ce3dd1a5e67a481d9528d89
Karan Bisht <karan161003@gmail.com>
2024-07-08 16:36:01 +0530

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -12,18 +12,18 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware setup
-app.use(express.json({limit: "25mb"}));
-app.use(express.urlencoded({extended: true, limit: "25mb"}));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true, limit: "25mb" }));
 // app.use(express.json());
 // app.use(express.urlencoded({  }));
 app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 
 // cors() must be present above routes
-const corsOptions = { 
-    origin : process.env.FRONT_END_DOMAIN, 
-}    
-app.use(cors(corsOptions)) 
+const corsOptions = {
+    origin: [process.env.FRONT_END_DOMAIN_1, process.env.FRONT_END_DOMAIN_2, process.env.FRONT_END_DOMAIN_3],
+}
+app.use(cors(corsOptions))
 // app.use(cors());
 
 import mongoose from "mongoose";
